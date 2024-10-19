361d2262d46e9aa099454b7c7e83c98e90511377
Aleksandr <frolov85@bk.ru>
2024-10-19 17:38:02 +0300

    delete ErrorResponse

--- src/main/java/com/example/Tasktracker/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Tasktracker.exception;
-
-public class EntityNotFoundException extends Exception {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
